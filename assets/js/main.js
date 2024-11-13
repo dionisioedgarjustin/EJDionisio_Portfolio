1bc1e28e5ae25733aace34272975f62f3d477a38
dionisioedgarjustin <edgarjustin.dionisio@tup.edu.ph>
2024-11-14 03:50:48 +0800

    Hide Preloader & Footer and  Page (About)
    I put the hider for preloader in js and I add the animations for footer and the about page of my portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+jQuery(window).on('load', function() {
+        
+    // HIDE PRELAODER
+    $(".preloader").addClass("preloader-hidden");
+
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
+
+        $(".hero .animation-container").each(function(){
+
+            var e = $(this);
+
+            setTimeout(function(){
+
+                e.addClass("run-animation");
+
+            }, e.data("animation-delay") );
+
+        });
+
+    }, 900 );
+
+});
+
